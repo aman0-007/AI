3fddeab81f0d209b36031ae7eb95497da12c00cd
aman0-007 <dwivedi.aman2811@gmail.com>
2025-01-13 10:47:55 +0530

    deci added

--- decision_tree_classifier.py
+++ decision_tree_classifier.py
@@ -38,12 +38,29 @@ y = partydata.Decision
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 1)
 
-clf = DecisionTreeClassifier(criterion = "entropy", max_depth = 5)
-clf = clf.fit(X_train, y_train)
+ada_party = DecisionTreeClassifier(criterion = "entropy", max_depth = 5)
+ada_party = ada_party.fit(X_train, y_train)
 
-y_pred = clf.predict(X_test)
+y_pred = ada_party.predict(X_test)
 
 print(f"ytest = {y_test}")
 print(f"y_pred = {y_pred}")
 
 print(f"Accuracy : {metrics.accuracy_score(y_test, y_pred)}")
+
+dot_data = StringIO()
+export_graphviz(ada_party, out_file = dot_data, filled = True, rounded = True, feature_names= feature_cols, class_names = ['attending','not attending'])
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+graph.write_png('party.png')
+
+new_data = [[
+    int(input("Are you invited to the party? (1 = Yes, 2 = No): ")),
+    int(input("Are you feeling happy or sad? (1 = Happy, 2 = Sad): ")),
+    int(input("How's the weather? (1 = Nice, 2 = Bad): ")),
+    int(input("What’s the size of the party? (1 = Small, 2 = Big): "))
+]]
+print(new_data)
+
+y_new_pred = ada_party.predict(new_data)
+
+print(f"y_new_pred = {y_new_pred}")
